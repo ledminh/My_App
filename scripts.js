1fbd7672c421681514ab46d74f1863a4254844b0
Minh Le <minh.le@ledminh.dev>
2025-02-21 22:01:25 -0500

    Add click event listenner.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 import NOTES from "./notes.js";
 
+const MODAL = document.getElementById("modal");
+const ADD_NOTE_BUTTON = document.getElementById("add-note-button");
+
 function init() {
+  ADD_NOTE_BUTTON.addEventListener("click", addNewNodeHandler);
+
   NOTES.render();
 }
 
 init();
+
+/**************************/
+
+function addNewNodeHandler() {
+  MODAL.style.display = "block";
+}
